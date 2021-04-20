916b80944420544fef8a351e2ddf2f495433c105
MennaElhelaly <manonaelhelaly@gmail.com>
2021-04-20 15:15:11 -0700

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Menna Elhelaly on 4/20/21.
+//  Copyright © 2021 Menna Elhelaly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SportsAppTests/SportsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SportsAppTests.swift
+//  SportsAppTests
+//
+//  Created by Menna Elhelaly on 4/20/21.
+//  Copyright © 2021 Menna Elhelaly. All rights reserved.
+//
+
+import XCTest
+@testable import SportsApp
+
+class SportsAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
