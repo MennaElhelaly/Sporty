11b9b9f0334aae7c5c06e1a2e2e6ae9d5843b291
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2021-04-21 23:07:25 +0200

    Leaguesvs design with dami data

---
+++ SportsApp/LeaguesDataClass.swift
@@ -0,0 +1,9 @@
+//
+//  LeaguesDataClass.swift
+//  SportsApp
+//
+//  Created by Ayman Omara on 21/04/2021.
+//  Copyright © 2021 Menna Elhelaly. All rights reserved.
+//
+
+import Foundation
---
+++ SportsApp/LeaguesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaguesTableViewCell.swift
+//  SportsApp
+//
+//  Created by Ayman Omara on 21/04/2021.
+//  Copyright © 2021 Menna Elhelaly. All rights reserved.
+//
+
+import UIKit
+
+class LeaguesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
